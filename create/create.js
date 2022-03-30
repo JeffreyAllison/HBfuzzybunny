ff401a2d3d027e996a9cf2c842312494cc888e94
JeffAllison <itsjallison@gmail.com>
2022-03-30 14:30:52 -0700

    completed getFamilies, deleteBunny, and createBunny functions in fetch.js.  completed displayFamilies function in families.js

--- create/create.js
+++ create/create.js
@@ -1,41 +1,44 @@
-import { 
-    createBunny, 
-    getFamilies, 
-    checkAuth, 
-    logout 
+import {
+  createBunny,
+  getFamilies,
+  checkAuth,
+  logout
 } from '../fetch-utils.js';
 
 const form = document.querySelector('.bunny-form');
 const logoutButton = document.getElementById('logout');
 
-form.addEventListener('submit', async e => {
-    // prevent default
+form.addEventListener('submit', async (e) => {
+  // prevent default
+  e.preventDefault();
 
-    // get the name and family id from the form
+  // get the name and family id from the form
+  const data = new FormData(form);
 
-    // use createBunny to create a bunny with this name and family id
-    
-    form.reset();
+  // use createBunny to create a bunny with this name and family id
+  await createBunny(data.get('name'), data.get('family_id'));
+
+  form.reset();
 });
 
-window.addEventListener('load', async() => {
-    // let's dynamically fill in the families dropdown from supabase
-    // grab the select HTML element from the DOM
+window.addEventListener('load', async () => {
+  // let's dynamically fill in the families dropdown from supabase
+  // grab the select HTML element from the DOM
 
-    // go get the families from supabase
+  // go get the families from supabase
 
-    // for each family
+  // for each family
 
-    // create an option tag
+  // create an option tag
 
-    // set the option's value and text content
+  // set the option's value and text content
 
-    // and append the option to the select
+  // and append the option to the select
 });
 
 
 checkAuth();
 
 logoutButton.addEventListener('click', () => {
-    logout();
+  logout();
 });
--- fetch-utils.js
+++ fetch-utils.js
@@ -1,63 +1,77 @@
+/* eslint-disable no-console */
 // Create your own supabase database using the provided seeds.sql file
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://gxwgjhfyrlwiqakdeamc.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-export function getUser() {
-    return client.auth.session() && client.auth.session().user;
+export function getUser () {
+  return client.auth.session() && client.auth.session().user;
 }
 
-export async function getFamilies() {
-    // fetch all families and their bunnies
+export async function getFamilies () {
+  // fetch all families and their bunnies
+  const response = await client
+    .from('loving_families')
+    .select('*, fuzzy_bunnies (*)');
 
-    return checkError(response);
+  return checkError(response);
 }
 
-export async function deleteBunny(id) {
-    // delete a single bunny using the id argument
+export async function deleteBunny (id) {
+  // delete a single bunny using the id argument
+  const response = await client
+    .from('fuzzy_bunnies')
+    .delete(id)
+    .match({ user_id: client.auth.user().id });
 
-    return checkError(response);
+  return checkError(response);
 }
 
-export async function createBunny(bunny) {
-    // create a bunny using the bunny argument
+export async function createBunny (bunny, family_id) {
+  // create a bunny using the bunny argument
+  const response = await client
+    .from('fuzzy_bunnies')
+    .insert({
+      name: bunny,
+      family_id: family_id
+    });
 
-    return checkError(response);
+  return checkError(response);
 }
 
 // MARTHA STEWART (PRE-MADE) FUNCTIONS
 
-export async function checkAuth() {
-    const user = getUser();
+export async function checkAuth () {
+  const user = getUser();
 
-    if (!user) location.replace('../');
+  if (!user) location.replace('../');
 }
 
-export async function redirectIfLoggedIn() {
-    if (getUser()) {
-        location.replace('./families');
-    }
+export async function redirectIfLoggedIn () {
+  if (getUser()) {
+    location.replace('./families');
+  }
 }
 
-export async function signupUser(email, password) {
-    const response = await client.auth.signUp({ email, password });
+export async function signupUser (email, password) {
+  const response = await client.auth.signUp({ email, password });
 
-    return response.user;
+  return response.user;
 }
 
-export async function signInUser(email, password) {
-    const response = await client.auth.signIn({ email, password });
+export async function signInUser (email, password) {
+  const response = await client.auth.signIn({ email, password });
 
-    return response.user;
+  return response.user;
 }
 
-export async function logout() {
-    await client.auth.signOut();
+export async function logout () {
+  await client.auth.signOut();
 
-    return (window.location.href = '../');
+  return (window.location.href = '../');
 }
 
-function checkError({ data, error }) {
-    return error ? console.error(error) : data;
+function checkError ({ data, error }) {
+  return error ? console.error(error) : data;
 }
